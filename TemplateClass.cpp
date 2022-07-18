4ee364aa667294ab666c25a2fa454369dfbbe9b3
Marat Dzodziev <minamota80@gmail.com>
2022-07-18 19:35:43 +0300

    First commit

---
+++ TemplateClass.cpp
@@ -0,0 +1,12 @@
+﻿// TemplateClass.cpp: определяет точку входа для приложения.
+//
+
+#include "TemplateClass.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
